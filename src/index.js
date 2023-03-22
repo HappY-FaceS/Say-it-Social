47c00153f4a10f610b368fe10cabdadefe3bacfc
HappY-FaceS <rishavkumarcgc@gmail.com>
2023-03-22 19:24:41 +0530

    Added Toggle likes Service

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser';
 
 import {connect} from './config/database.js';
 import apiRoutes from './routes/index.js';
-
+// import { UserRespository, TweetRepository } from './repository/index.js';
+// import LikeService from './services/like-service.js';
 
 
 const startServer = async () =>{
@@ -18,6 +19,26 @@ const startServer = async () =>{
     console.log('server started at',PORT);
     await connect();
     console.log('Mongo db connected');
+
+    // const userRepo = new UserRespository();
+    // const tweetRepo = new TweetRepository();
+    // const likeService = new LikeService();
+     
+    // const user = await userRepo.create({
+    //     email:'rishavkumarcgc@gmail.com' ,
+    //     password:'jaishreeram' ,
+    //     name:'Rishav' ,
+    //     username: 'DevGaii'
+    // });
+    // console.log(user);
+
+    
+    // const response = await likeService.toggleLike('6419df52498df991299a5f04', 'Tweet', '641aedb1c580c7968bff62f5');
+    // console.log(response);
+    
+
+
+
 });
 
 };
